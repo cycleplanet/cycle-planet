701ffa4a28f436fb23edb6b2cadc3d8cf5cb0fcf
bas idsinga <basidsinga@gmail.com>
2021-08-21 16:39:29 +0200

    change meta

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
 
 <script>
 import { mapActions } from "vuex";
+import meta from "src/utils/meta.js";
 
 export default {
   methods: {
@@ -19,6 +20,67 @@ export default {
     this.handleAuthStateChange();
     this.loadBaseData();
     this.loadSecondaryData();
+  },
+  meta() {
+    return {
+      title: "Cycle Planet",
+      titleTemplate: (title) => `${title} | The bicycle touring guide`,
+
+      // meta tags
+      meta: {
+        description: { name: "description", content: "Page 1" },
+        keywords: {
+          name: "keywords",
+          content:
+            "bicycle, bike, cycle, cycling, touring, visa, border, safe, information",
+        },
+        equiv: {
+          "http-equiv": "Content-Type",
+          content: "text/html; charset=UTF-8",
+        },
+        // note: for Open Graph type metadata you will need to use SSR, to ensure page is rendered by the server
+        ogTitle: {
+          name: "og:title",
+          // optional; similar to titleTemplate, but allows templating with other meta properties
+          template(ogTitle) {
+            return `${ogTitle}`;
+          },
+        },
+      },
+
+      // CSS tags
+      link: {
+        material: {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+        },
+      },
+
+      // JS tags
+      script: {
+        ldJson: {
+          type: "application/ld+json",
+          innerHTML: `{ "@context": "http://schema.org" }`,
+        },
+      },
+
+      // <html> attributes
+      htmlAttr: {
+        "xmlns:cc": "http://creativecommons.org/ns#", // generates <html xmlns:cc="http://creativecommons.org/ns#">,
+        empty: undefined, // generates <html empty>
+      },
+
+      // <body> attributes
+      bodyAttr: {
+        "action-scope": "xyz", // generates <body action-scope="xyz">
+        empty: undefined, // generates <body empty>
+      },
+
+      // <noscript> tags
+      noscript: {
+        default: "This is content for browsers with no JS (or disabled JS)",
+      },
+    };
   },
 };
 </script>
